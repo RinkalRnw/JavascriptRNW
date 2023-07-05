8187f7c133a2cd7bc9f225ea7fd248ae95cedb37
RinkalRnw <rinkalsoni161@gmail.com>
2023-07-05 11:01:35 +0530

    product

--- FinalProject/Admin/product.js
+++ FinalProject/Admin/product.js
@@ -1,3 +1,4 @@
+disp();
 //Get Category data into dropdown
 let catdata = JSON.parse(localStorage.getItem("CategoryInfo"));
 let row = '';
@@ -9,23 +10,71 @@ if(catdata != null && catdata.Category.length > 0){
     }
     document.getElementById("catid").innerHTML = row;
 }
-
-//Insert /add product data to localstorage
-document.getElementById("btn2").addEventListener("click",function(){
-    let pimage = document.getElementsByName("pimg");
+//Onchange image store in localstorage
+document.getElementById("pimg").addEventListener("change",()=>{
+    let pimage = document.getElementById("pimg");
     
     if(pimage.files && pimage.files[0]){
         let r = new FileReader();
         r.readAsDataURL(pimage.files[0]);
         r.addEventListener("load",()=>{
-            console.log("image is  "+r.result);
-            localStorage.setItem("ProductImage",r.result);
-            document.getElementById("prod_img").src = r.result;
-
+            localStorage.setItem("ProductImage",r.result);           
         });
-
-       
-    } else {
-        console.log('jk');
     }
+});
+//Insert /add product data to localstorage
+document.getElementById("btn2").addEventListener("click",function(){
+    
+    let catid = document.prodfrm.catid.value;
+    let pname = document.prodfrm.pname.value;
+    let price = document.prodfrm.price.value;
+    let pimage = localStorage.getItem("ProductImage");
+
+    let pdata = {};
+    let data = {
+        catid:catid,
+        pid:1,
+        name:pname,
+        price:price,
+        image:pimage
+    };
+    pdata.productInfo = [data];
+    localStorage.setItem("Productdata",JSON.stringify(pdata)); 
+    disp();  
 }); 
+
+function disp(){
+    let tr="";
+    tr+="<tr>";
+    tr+="<th> ID </th>"
+    tr+="<th> catid </th>"
+    tr+="<th> Name </th>"
+    tr+="<th> Price </th>"
+    tr+="<th> Image </th>"
+    tr+="<th> Action </th>"
+    tr+="</tr>"
+  let p= JSON.parse(localStorage.getItem("Productdata"));
+  for(let i=0;i<p.productInfo.length; i++){
+    tr+="<tr>"
+    tr+="<td>"+p.productInfo[i].pid+"</td>"
+    tr+="<td>"+p.productInfo[i].catid+"</td>"
+    tr+="<td>"+p.productInfo[i].name+"</td>"
+    tr+="<td>"+p.productInfo[i].price+"</td>"
+    tr+="<td><img src='"+p.productInfo[i].image+"'></td>"
+    tr+="<td><input type='button' id='deldata' name='deldata' value= 'delete' onclick='deldata("+p.productInfo[i].pid +")'></td>"
+    tr+="</tr>"
+  }
+  document.getElementById("tabid").innerHTML=tr;
+   
+
+}
+
+function deldata(id){
+  let p= JSON.parse(localStorage.getItem("Productdata"));
+    if(p!= null){
+        let id1 =id-1;
+        p.productInfo.splice(id1,1);
+        localStorage.setItem("Productdata",JSON.stringify(p));
+        disp();
+    }
+}
